5b8e035b8a406d789d48af20430be9c64a613e01
tuancao <tuan.cao@jmango360.com>
2021-06-14 17:59:50 +0700

    b

--- App.js
+++ App.js
@@ -1,21 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StatusBar, View, ActivityIndicator, StyleSheet } from 'react-native';
+import { Asset } from 'expo';
 
-export default function App() {
+import { Discovery } from './components';
+
+const stories = [
+  {
+    id: '2',
+    source: require('./assets/stories/2.jpg'),
+    user: 'derek.russel',
+    avatar: require('./assets/avatars/derek.russel.png'),
+  },
+  {
+    id: '4',
+    source: require('./assets/stories/4.jpg'),
+    user: 'jmitch',
+    avatar: require('./assets/avatars/jmitch.png'),
+  },
+  {
+    id: '7',
+    source: require('./assets/stories/7.jpg'),
+    user: 'andrea.schmidt',
+    avatar: require('./assets/avatars/andrea.schmidt.png'),
+    video: require('./assets/stories/7.mp4'),
+  },
+  {
+    id: '5',
+    source: require('./assets/stories/5.jpg'),
+    user: 'monicaa',
+    avatar: require('./assets/avatars/monicaa.png'),
+  },
+  {
+    id: '3',
+    source: require('./assets/stories/3.jpg'),
+    user: 'alexandergarcia',
+    avatar: require('./assets/avatars/alexandergarcia.png'),
+  },
+  {
+    id: '1',
+    source: require('./assets/stories/1.jpg'),
+    user: 'andrea.schmidt',
+    avatar: require('./assets/avatars/andrea.schmidt.png'),
+  },
+  {
+    id: '6',
+    source: require('./assets/stories/6.jpg'),
+    user: 'andrea.schmidt',
+    avatar: require('./assets/avatars/andrea.schmidt.png'),
+  },
+];
+
+type AppState = {
+  ready: boolean,
+};
+
+const App = () => {
+  // const [ready, setReady] = useState(true);
+  let ready = true;
+  // useEffect(async() => {
+  //   await Promise.all(
+  //     stories.map((story) =>
+  //       Promise.all([
+  //         Asset.loadAsync(story.source),
+  //         Asset.loadAsync(story.avatar),
+  //         story.video ? Asset.loadAsync(story.video) : undefined,
+  //       ])
+  //     )
+  //   );
+  //   setReady(true);
+  // }, []);
+
+  if (!ready) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size='large' color='black' />
+      </View>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <StatusBar barStyle='light-content' backgroundColor='black' />
+      <Discovery {...{ stories }} />
     </View>
   );
-}
-
+};
+export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
   },
 });
